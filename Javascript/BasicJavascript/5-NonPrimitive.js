4f13c8d916f74993ba211e50e392c450c41057ba
Shrutika_Dhanad <dhanadshrutika@gmail.com>
2026-08-20 14:17:05 +0530

    non primitive datatypes

---
+++ Javascript/BasicJavascript/5-NonPrimitive.js
@@ -0,0 +1,18 @@
+// arrays :
+const heros = ["shaktiman", "nagraj"];
+
+// Objects:
+let objects = {
+  name: "shrutika",
+  age: 21,
+};
+console.log(typeof objects);
+
+const myFucntion = function () {
+  console.log("shrutika");
+};
+myFucntion();
+console.log(typeof myFucntion); //function-object
+
+let bigNum = 142587996689625896n;
+console.log(typeof bigNum);
