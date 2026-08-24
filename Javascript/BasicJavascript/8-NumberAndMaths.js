bc6149f2fbcd9c458d76df3377147b2878b979f8
Shrutika_Dhanad <dhanadshrutika@gmail.com>
2026-08-24 22:09:00 +0530

    numbers and maths in js

---
+++ Javascript/BasicJavascript/8-NumberAndMaths.js
@@ -0,0 +1,12 @@
+//conver number into string
+
+let balance = new Number(500);
+console.log(balance);
+console.log(balance.toString().length);
+
+//use of precison value
+
+console.log(balance.toFixed(1));
+
+let num = 123.654;
+console.log(num.toPrecision(4));
