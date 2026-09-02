eea5d3a74c3c558ffa73546afa67d4118065d29f
Shrutika_Dhanad <dhanadshrutika@gmail.com>
2026-09-02 19:19:53 +0530

    function in Javascript

---
+++ Javascript/BasicJs2/Functions/01_func_basic.js
@@ -0,0 +1,16 @@
+function printsum(num1, num2) {
+  let res = num1 + num2;
+  return res;
+}
+const res = printsum(10, 50);
+console.log("result of sum:" + res);
+
+const ShrutiFunction = (salary, location) => {
+  let name = "shrutika";
+  let age = 21;
+  console.log("my name is " + name + " and age is :" + 21);
+
+  
+  console.log("salary is  :  " + salary + "  location " + location)
+};
+console.log(ShrutiFunction(500000, "Chinchwad"));
