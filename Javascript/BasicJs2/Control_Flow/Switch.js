5dfe615855f129c511ef7dc778449f9958f3b863
Shrutika_Dhanad <dhanadshrutika@gmail.com>
2026-09-05 17:34:04 +0530

    switch statement

---
+++ Javascript/BasicJs2/Control_Flow/Switch.js
@@ -0,0 +1,62 @@
+const month = 5;
+switch (month) {
+  case 1:
+    console.log("january");
+    break;
+  case 2:
+    console.log("feb");
+    break;
+  case 3:
+    console.log("march");
+    break;
+  case 4:
+    console.log("april");
+    break;
+  case 5:
+    console.log("may");
+    break;
+  case 6:
+    console.log("june");
+    break;
+  case 7:
+    console.log("july");
+    break;
+  case 8:
+    console.log("aug");
+    break;
+  case 9:
+    console.log("sept");
+    break;
+  case 10:
+    console.log("aucto");
+    break;
+  case 11:
+    console.log("nov");
+    break;
+  case 12:
+    console.log("dec");
+    break;
+
+  default:
+    console.log("not valid");
+    break;
+}
+
+const months = "feb";
+switch (months) {
+  case "january":
+    console.log("1");
+    break;
+  case "feb":
+    console.log("2");
+    break;
+  case "march":
+    console.log("3");
+    break;
+  case "april":
+    console.log("4");
+    break;
+  default:
+    console.log("not valid");
+    break;
+}
