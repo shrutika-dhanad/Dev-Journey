bd28724f1556452b2dea56f1d2f8fac7b68c3c58
Shrutika_Dhanad <dhanadshrutika@gmail.com>
2026-09-08 15:31:51 +0530

    while and do-while loop

---
+++ Javascript/BasicJs2/Iterations/02_While_and_do_while.js
@@ -0,0 +1,35 @@
+let num = 0;
+while (num <= 10) {
+  console.log(num);
+  num = num + 1;
+}
+
+let myArry = [
+  "shrutika",
+  "vaibhavi",
+  "priitesh",
+  "Raj",
+  "Aditya",
+  "sayali",
+  "tejashri",
+];
+let arr = 0;
+while (arr < myArry.length) {
+  console.log(myArry[arr]);
+  arr = arr + 1;
+}
+
+//do-while
+
+let marks = 1;
+do {
+//   console.log(`marks is : ${marks}`);
+  marks++;
+} while (marks <= 10);
+
+let score=11;
+do{
+    console.log(`score is ${score}`);
+    score++
+} 
+while(score<=5);
